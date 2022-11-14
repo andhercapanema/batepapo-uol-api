d116c6d4352288880a5eee7a06c09e515063e19c
andherpaulo <andher.dev@gmail.com>
2022-11-14 14:01:00 -0300

    Adição do post '/messages' que permite o envio que mensagens para o servidor

--- src/index.js
+++ src/index.js
@@ -10,57 +10,69 @@ const userSchema = joi.object({
     name: joi.string().required().min(3).trim(),
 });
 
+const messageSchema = joi.object({
+    to: joi.string().required().trim(),
+    text: joi.string().required().trim(),
+    type: joi.valid("message", "private_message"),
+});
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 dotenv.config();
 const mongoClient = new MongoClient(process.env.MONGO_URI);
-let db, users, messages;
+let db, usersCollection, messagesCollection;
 
 try {
     await mongoClient.connect();
     db = mongoClient.db("batePapoUol");
-    users = db.collection("users");
-    messages = db.collection("messages");
+    usersCollection = db.collection("users");
+    messagesCollection = db.collection("messages");
 } catch (err) {
     console.error(err);
 }
 
-async function userIsLogged(name) {
+async function userIsLogged(name, res) {
     try {
-        const onlineUsers = await users.find().toArray();
+        const onlineUsers = await usersCollection.find().toArray();
         return onlineUsers
             .map((user) => user.name)
             .some((onlineName) => onlineName === name);
     } catch (err) {
         console.error(err);
+        res.sendStatus(500);
     }
+}
+
+function newStandardTime() {
+    return format(new Date(), "HH:mm:ss", { locale: ptBR });
 }
 
 async function userLogin(name, res) {
     try {
-        await users.insertOne({
+        await usersCollection.insertOne({
             name,
             lastStatus: new Date(),
         });
 
-        await messages.insertOne({
+        await messagesCollection.insertOne({
             from: name,
             to: "Todos",
             text: "entra na sala...",
             type: "status",
-            time: format(new Date(), "HH:mm:ss", { locale: ptBR }),
+            time: newStandardTime(),
         });
 
         res.sendStatus(201);
     } catch (err) {
-        res.status(500).send(err);
+        console.error(err);
+        res.sendStatus(500);
     }
 }
 
 app.post("/participants", async (req, res) => {
-    const userValidation = userSchema.validate(
+    const { value, error } = userSchema.validate(
         { name: req.body.name },
         {
             abortEarly: false,
@@ -68,14 +80,14 @@ app.post("/participants", async (req, res) => {
         }
     );
 
-    if (userValidation.error !== undefined)
+    if (error !== undefined)
         return res
             .status(422)
-            .send(userValidation.error.details.map((detail) => detail.message));
+            .send(error.details.map((detail) => detail.message));
 
-    const { name } = userValidation.value;
+    const { name } = value;
 
-    if (await userIsLogged(name)) {
+    if (await userIsLogged(name, res)) {
         res.sendStatus(409);
     } else {
         userLogin(name, res);
@@ -84,15 +96,59 @@ app.post("/participants", async (req, res) => {
 
 app.get("/participants", async (req, res) => {
     try {
-        const onlineUsers = await users.find().toArray();
+        const onlineUsers = await usersCollection.find().toArray();
         res.send(onlineUsers);
     } catch (err) {
-        res.status(500).send(err);
+        console.error(err);
+        res.sendStatus(500);
     }
 });
 
-app.post("/test", (req, res) => {
-    res.send({ date: format(new Date(), "HH:mm:ss", { locale: ptBR }) });
+app.post("/messages", async (req, res) => {
+    let { to, text, type } = req.body;
+    const { user } = req.headers;
+
+    try {
+        const userIsLogged = await usersCollection.findOne({ name: user });
+
+        if (userIsLogged === null)
+            return res
+                .status(422)
+                .send({ message: "Usuário remetente não está logado!" });
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+
+    const { value, error } = messageSchema.validate(
+        { to, text, type },
+        {
+            abortEarly: false,
+            convert: true,
+        }
+    );
+
+    if (error !== undefined)
+        return res
+            .status(422)
+            .send(error.details.map((detail) => detail.message));
+
+    ({ to, text, type } = value);
+
+    try {
+        await messagesCollection.insertOne({
+            from: user,
+            to,
+            text,
+            type,
+            time: newStandardTime(),
+        });
+
+        res.sendStatus(201);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
 });
 
 app.listen(5000, () => console.log("Server running in port: 5000"));
